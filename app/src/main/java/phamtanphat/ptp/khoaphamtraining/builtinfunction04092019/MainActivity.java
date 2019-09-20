4a990a7db1b2c17c977633639801055daac64b25
phatdroid94 <phatdroid94@gmail.com>
2019-09-20 21:06:55 +0700

    ham lam tron

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/builtinfunction04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/builtinfunction04092019/MainActivity.java
@@ -1,9 +1,9 @@
 package phamtanphat.ptp.khoaphamtraining.builtinfunction04092019;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,50 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // method == function
-        // Giai quyet van de lap code
-        //unboxing
-        // auto boxing
-        // Task :
-            // 1 : Chay 0 - 100
-            // 2 : Dieu dung thi in ra log
-        // function insochan
-        //function insole
-        //function insochia3du1
-        inso(1 ,3);
-    }
-    // Thuong duoc tao trong class
-    // 1 : Access modifier : Pham vi truy cap
-    // public : O dau cung thay duoc het
-    // private : tao ra dau thi dung o do
-    // 2 : Gia tri tra ve
-    //3 : ten phuong thuc
-    private void inso(int sodu , int sobichia){
-        for (int i = 0 ; i<= 100 ; i++){
-            if (i % sobichia == sodu){
-                Log.d("BBB",i + "");
-            }
+        // Random
+        // 0 - 5
+        for (int i = 0 ; i < 10 ; i++){
+            double value = Math.round(Math.random() * 5);
+            Log.d("BBB",value + "");
         }
+        // lam tron gia tri
+//        Math.floor() : lam tron xuong
+//        Math.ceil() : lam tron len
+        double number1 = Math.floor(4.9999999);
+        double number2 = Math.ceil(4.01);
+        double number3 = Math.round(4.5);
+        Log.d("BBB",number3 + "");
     }
-    private void insoChan(){
-        for (int i = 0 ; i<= 100 ; i++){
-            if (i % 2 == 0){
-                Log.d("BBB",i + "");
-            }
-        }
-    }
-    private void insoLe(){
-        for (int i = 0 ; i<= 100 ; i++){
-            if (i % 2 == 1){
-                Log.d("BBB",i + "");
-            }
-        }
-    }
-    private void insoChia3du1(){
-        for (int i = 0 ; i<= 100 ; i++){
-            if (i % 3 == 1){
-                Log.d("BBB",i + "");
-            }
-        }
-    }
+
 }
