290d25fa9ebcab6f1a011d6c19740e166355dd1f
phatdroid94 <phatdroid94@gmail.com>
2019-09-20 20:39:59 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/builtinfunction04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/builtinfunction04092019/MainActivity.java
@@ -15,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
         // Giai quyet van de lap code
         //unboxing
         // auto boxing
-        int ketqua = tinhTong(5,10);
-        Log.d("BBB",ketqua +"");
+        // Task :
+            // 1 : Chay 0 - 100
+            // 2 : Dieu dung thi in ra log
+        // function insochan
+        //function insole
+        //function insochia3du1
+        insoChan();
     }
     // Thuong duoc tao trong class
     // 1 : Access modifier : Pham vi truy cap
@@ -24,8 +29,25 @@ public class MainActivity extends AppCompatActivity {
     // private : tao ra dau thi dung o do
     // 2 : Gia tri tra ve
     //3 : ten phuong thuc
-    private Integer tinhTong(int a , int b){
-        int tong = a + b;
-        return tong;
+    private void insoChan(){
+        for (int i = 0 ; i<= 100 ; i++){
+            if (i % 2 == 0){
+                Log.d("BBB",i + "");
+            }
+        }
+    }
+    private void insoLe(){
+        for (int i = 0 ; i<= 100 ; i++){
+            if (i % 2 == 1){
+                Log.d("BBB",i + "");
+            }
+        }
+    }
+    private void insoChia3du1(){
+        for (int i = 0 ; i<= 100 ; i++){
+            if (i % 3 == 1){
+                Log.d("BBB",i + "");
+            }
+        }
     }
 }
